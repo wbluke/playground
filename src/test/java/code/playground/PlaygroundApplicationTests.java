86c0b2d21386c829353c507743f762694f71af6f
wbluke <pwb0127@gmail.com>
2020-07-30 11:20:14 +0900

    spring boot initialize

---
+++ src/test/java/code/playground/PlaygroundApplicationTests.java
@@ -0,0 +1,13 @@
+package code.playground;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlaygroundApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
