2eb14e50d552a4b7c666d4012bec083244860824
wbluke <pwb0127@gmail.com>
2020-07-31 21:09:11 +0900

    로그 프로젝트 구축을 위한 로그 모듈 세팅

--- src/main/java/playground/logging/PlaygroundApplication.java
+++ src/main/java/playground/logging/PlaygroundApplication.java
@@ -1,4 +1,4 @@
-package code.playground;
+package playground.logging;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/playground/logging/PlaygroundApplicationTests.java
+++ src/test/java/playground/logging/PlaygroundApplicationTests.java
@@ -1,4 +1,4 @@
-package code.playground;
+package playground.logging;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
