f60513edd816b71bef78bbed5435710eec1a5c4c
wbluke <pwb0127@gmail.com>
2020-09-11 22:35:20 +0900

    Junit4 의존성 제거

---
+++ logging-system/src/test/java/com/wbluke/playground/logging/LoggingSystemApplicationTest.java
@@ -0,0 +1,13 @@
+package com.wbluke.playground.logging;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoggingSystemApplicationTest {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
