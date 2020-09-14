796e1da8bbe3ac9391ccd7dc13f10119577f9f1d
wbluke <pwb0127@gmail.com>
2020-09-14 21:59:47 +0900

    local MySQL 설치 및 Batch Application과 연결

--- playground-batch/src/main/java/com/wbluke/playground/BatchApplication.java
+++ playground-batch/src/main/java/com/wbluke/playground/BatchApplication.java
@@ -1,15 +1,19 @@
 package com.wbluke.playground;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Slf4j
 @EnableBatchProcessing
 @SpringBootApplication
 public class BatchApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(BatchApplication.class, args);
+        int exit = SpringApplication.exit(SpringApplication.run(BatchApplication.class, args));
+        log.info("exitCode={}", exit);
+        System.exit(exit);
     }
 
 }
--- playground-batch/src/main/java/com/wbluke/playground/batch/SimpleScopeCheckBatch.java
+++ playground-batch/src/main/java/com/wbluke/playground/batch/SimpleScopeCheckBatch.java
@@ -5,9 +5,12 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.JobScope;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.item.ItemReader;
 import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -29,11 +32,14 @@ public class SimpleScopeCheckBatch {
     @Bean(BEAN_PREFIX + "job")
     public Job simpleScopeCheckJob() {
         return jobBuilderFactory.get(JOB_NAME)
-                .start(simpleStep())
+//                .start(simpleStep())
+//                .next(simpleTaskletStep())
+                .start(simpleTaskletStep())
                 .build();
     }
 
-    @Bean(BEAN_PREFIX + "job")
+    @Bean(BEAN_PREFIX + "simpleStep")
+    @JobScope
     public Step simpleStep() {
         return stepBuilderFactory.get("simpleStep")
                 .chunk(chunkSize)
@@ -43,6 +49,7 @@ public class SimpleScopeCheckBatch {
     }
 
     @Bean(BEAN_PREFIX + "simpleReader")
+    @StepScope
     public ItemReader<Object> simpleReader() {
         log.info("Simple Reader Start");
 
@@ -50,12 +57,24 @@ public class SimpleScopeCheckBatch {
     }
 
     @Bean(BEAN_PREFIX + "simpleWriter")
+    @StepScope
     public ItemWriter<Object> simpleWriter() {
         log.info("Simple Writer Start");
 
         return items -> {
 
         };
+    }
+
+    @Bean(BEAN_PREFIX + "simpleTaskletStep")
+    @JobScope
+    public Step simpleTaskletStep() {
+        return stepBuilderFactory.get("simpleTaskletStep")
+                .tasklet((contribution, chunkContext) -> {
+                    log.info("Simple Tasklet Start");
+
+                    return RepeatStatus.FINISHED;
+                }).build();
     }
 
 }
