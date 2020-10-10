f2d1174525c6b6aeee32f75dd61aa7a13b55d485
wbluke <pwb0127@gmail.com>
2020-10-10 19:06:24 +0900

    logging level 별 endpoint 추가

--- playground-logging-system/src/main/java/com/wbluke/playground/logging/LoggingController.java
+++ playground-logging-system/src/main/java/com/wbluke/playground/logging/LoggingController.java
@@ -1,9 +1,11 @@
 package com.wbluke.playground.logging;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 public class LoggingController {
 
@@ -18,6 +20,30 @@ public class LoggingController {
     @GetMapping("/health")
     public String checkHealth() {
         return "healthy";
+    }
+
+    @GetMapping("/log/error")
+    public String logError() {
+        log.error("log error");
+        return "error";
+    }
+
+    @GetMapping("/log/warn")
+    public String logWarn() {
+        log.warn("log warn");
+        return "warn";
+    }
+
+    @GetMapping("/log/info")
+    public String logInfo() {
+        log.info("log info");
+        return "info";
+    }
+
+    @GetMapping("/log/debug")
+    public String logDebug() {
+        log.debug("log debug");
+        return "debug";
     }
 
 }
